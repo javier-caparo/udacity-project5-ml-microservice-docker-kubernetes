b5045b9990b6d6651b47808510cf181f6768f7c4
EC2 Default User <ec2-user@ip-172-31-85-38.ec2.internal>
2020-08-13 02:47:11 +0000

    improvements adding json files and make_prediction.sh files

--- make_prediction.sh
+++ make_prediction.sh
@@ -3,5 +3,8 @@
 PORT=8000
 echo "Port: $PORT"
 
-# POST method predict with multiple predictions
-curl -X POST -H "Content-Type: application/json" -d @predictions.json http://localhost:$PORT/predict
+# First POST method predict
+curl -d @prediction_1.json -X POST -H "Content-Type: application/json"  http://localhost:$PORT/predict
+
+# 2do POST method predict
+curl -d @prediction_2.json -X POST -H "Content-Type: application/json"  http://localhost:$PORT/predict

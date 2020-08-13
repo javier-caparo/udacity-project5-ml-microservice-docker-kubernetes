d8e5ffeb352378c1d4e14dfa5aa28ada44ec9faa
Javier Caparo <javier.caparo@gmail.com>
2020-08-13 05:37:05 +0000

    updating run_kubernetes,sh file

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -9,15 +9,15 @@ export dockerpath=javiercaparo/udacity-project-ml-microservice-docker-kubernetes
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-sudo kubectl run ml-microservice --image=$dockerpath --port=80 --labels="app=ml-microservice"
+kubectl run ml-microservice --image=$dockerpath --port=80 --labels="app=ml-microservice"
 
 # Step 3:
 # List kubernetes pods
 sleep 10; echo "PODs creation in progress..just some seconds to be in running state"
-sleep 50; sudo kubectl get pods
+sleep 50; kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
 sleep 5; echo "Forwarding the container port to a host"
-export POD_NAME=$(sudo kubectl get pod -l app=ml-microservice -o jsonpath="{.items[0].metadata.name}")
-sudo kubectl port-forward $POD_NAME 8000:80
+export POD_NAME=$(kubectl get pod -l app=ml-microservice -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward $POD_NAME 8000:80
